05fd4cc86a40d09aa9b6bb110989bae1a452a851
Victor-Pizzaia <v_pizzaia@hotmail.com>
2022-05-03 14:44:07 -0300

    fix problems with functional test

--- src/test/java/com/br/pizzaia/tasks/functional/TasksTest.java
+++ src/test/java/com/br/pizzaia/tasks/functional/TasksTest.java
@@ -2,23 +2,23 @@ package com.br.pizzaia.tasks.functional;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TasksTest {
 	
 	public WebDriver acessarAplicacao() throws MalformedURLException {
 		// WebDriver driver = new ChromeDriver();
-		ChromeOptions options = new ChromeOptions();
-		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
-		driver.navigate().to("http://192.168.144.1:8001/tasks");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
+		driver.navigate().to("http://192.168.15.2:8001/tasks");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
 	
